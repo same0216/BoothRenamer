0c26aef30f640e8c823a72cc818cfb18309da9f4
same <ashida0216@yahoo.co.jp>
2023-04-30 19:33:47 +0900

    コメント追加

--- rename.js
+++ rename.js
@@ -1,6 +1,8 @@
 chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
+    // Boothのページ以外除外
     if (item.url.indexOf("https://booth.pm") === 0) {
         chrome.tabs.query({ active: true }, function (tabs) {
+            // ギフトページと自身の購入かで選別
             if(tabs[0].url.indexOf("https://accounts.booth.pm/orders") === 0) {
                 chrome.scripting.executeScript({
                     target: { tabId: tabs[0].id },
