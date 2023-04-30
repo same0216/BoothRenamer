f1bb08406a710d390db406b0067c2971183e3944
same <ashida0216@yahoo.co.jp>
2023-04-30 19:30:37 +0900

    ギフトページ対応

--- rename.js
+++ rename.js
@@ -1,16 +1,30 @@
 chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
     if (item.url.indexOf("https://booth.pm") === 0) {
         chrome.tabs.query({ active: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                function: function () {
-                    return document.querySelector("a[class='nav']").innerHTML;
-                }
-            }, function (content) {
-                let contentName = content[0].result
-                let replaceFilename = contentName + ".zip"
-                suggest({ filename: replaceFilename });
-            });
+            if(tabs[0].url.indexOf("https://accounts.booth.pm/orders") === 0) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: function () {
+                        return document.querySelector("a[class='nav']").innerHTML;
+                    }
+                }, function (content) {
+                    let contentName = content[0].result
+                    let replaceFilename = contentName + ".zip"
+                    suggest({ filename: replaceFilename });
+                });
+            } else {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: function () {
+                        return document.querySelector("div[class='text-black typograply-16 font-bold']").innerHTML;
+                    }
+                }, function (content) {
+                    let contentName = content[0].result
+                    let replaceFilename = contentName + ".zip"
+                    suggest({ filename: replaceFilename });
+                });
+            }
+            
         })
     } else {
         suggest({ filename: item.filename });
